53fa0c893eb390de2951127d6019a0d76845f8a3
fadhilthomas <mthomasfadhilayahya@gmail.com>
2021-08-22 04:56:00 +0700

    feat: update module

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/fadhilthomas/go-postgres-query-exporter/config"
 	"github.com/hpcloud/tail"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -21,6 +20,7 @@ func checkFileName(filename string, f chan<- string) {
 	for {
 		newFilename := fmt.Sprintf("%v", time.Now().Format(config.Get(config.LOG_FILE)+"postgresql-2006-01-02.log"))
 
+		// if detect new filename, send to channel
 		if newFilename != filename {
 			log.Debug().Str("file", "main").Msg(newFilename)
 			f <- newFilename
@@ -30,9 +30,8 @@ func checkFileName(filename string, f chan<- string) {
 }
 
 func listenLog() {
-	reSelect := regexp.MustCompile("duration: ([0-9]+.[0-9]+) ms {2}statement: (select|update|delete)")
-	reError := regexp.MustCompile("(error|fatal)")
-
+	var reSelect = regexp.MustCompile("duration: ([0-9]+.[0-9]+) ms {2}statement: (select|update|delete)")
+	var reError = regexp.MustCompile("(error|fatal)")
 	fileNameChan := make(chan string)
 
 	for {
@@ -45,8 +44,10 @@ func listenLog() {
 			continue
 		}
 
+		// send goroutine for checking filename in loop
 		go checkFileName(filename, fileNameChan)
 
+		// send goroutine that stops lines read in range
 		go func() {
 			for {
 				select {
@@ -68,6 +69,8 @@ func listenLog() {
 		for line := range logTail.Lines {
 			lineLow := strings.ToLower(line.Text)
 			ddlMatch := reSelect.MatchString(lineLow)
+
+			// check if log contain ddl query
 			if ddlMatch == true {
 				queryCounter.WithLabelValues(reSelect.FindStringSubmatch(lineLow)[2]).Inc()
 				duration, err := strconv.ParseFloat(reSelect.FindStringSubmatch(lineLow)[1], 64)
@@ -75,9 +78,10 @@ func listenLog() {
 					log.Error().Stack().Str("file", "main").Msg(err.Error())
 				}
 				queryHistogram.WithLabelValues(reSelect.FindStringSubmatch(lineLow)[2]).Observe(duration / 1000)
-				log.Debug().Str("file", "main").Msg(fmt.Sprintf("%s: %s", reSelect.FindStringSubmatch(lineLow)[2],reSelect.FindStringSubmatch(lineLow)[1]))
+				log.Debug().Str("file", "main").Msg(fmt.Sprintf("%s: %s", reSelect.FindStringSubmatch(lineLow)[1],reSelect.FindStringSubmatch(lineLow)[2]))
 			}
 
+			// check if log contain error
 			errMatch := reError.MatchString(lineLow)
 			if errMatch == true {
 				queryCounter.WithLabelValues(reError.FindStringSubmatch(lineLow)[1]).Inc()
@@ -87,9 +91,25 @@ func listenLog() {
 	}
 }
 
+func setLogLevel(){
+	debug := flag.Bool("debug", false, "sets log level to debug")
+	flag.Parse()
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+}
+
+func initMetric()  {
+	http.Handle("/metrics", promhttp.Handler())
+	prometheus.MustRegister(version)
+	prometheus.MustRegister(queryCounter)
+	prometheus.MustRegister(queryHistogram)
+}
+
 var (
 	appVersion = "1.0"
-	version    = promauto.NewGauge(prometheus.GaugeOpts{
+	version    = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "version",
 		Help: "Version information about this exporter",
 		ConstLabels: map[string]string{
@@ -111,24 +131,13 @@ var (
 	}, []string{"query"})
 )
 
-
 func main() {
-	debug := flag.Bool("debug", false, "sets log level to debug")
-	flag.Parse()
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-	if *debug {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	}
-
-	version.Set(1)
-	http.Handle("/metrics", promhttp.Handler())
-	prometheus.MustRegister(queryCounter)
-	prometheus.MustRegister(queryHistogram)
-
+	setLogLevel()
+	initMetric()
 	go listenLog()
 
 	log.Info().Str("file", "main").Msg("serving requests on port 9000")
-	err := http.ListenAndServe(":9000", nil)
+	err := http.ListenAndServe(":9080", nil)
 	if err != nil {
 		log.Error().Stack().Str("file", "main").Msg(err.Error())
 		return
